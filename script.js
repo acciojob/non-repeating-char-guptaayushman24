3b02874d6aeb6e33c4c16ae6aa50f24b74ac5cbd
Ayushman Gupta <125189298+guptaayushman24@users.noreply.github.com>
2026-01-04 09:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	
+   for (int i=0;i<str.length();i++) {
+  freq[i] = (freq[i] || 0) + 1;
+}
+	for (int i=0;i<freq.length;i++){
+		if (freq[i]==1){
+			return (char) (i + 'a'); 
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+ alert(firstNonRepeatedChar(input));
